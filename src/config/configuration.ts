ae77ebebfc222ce134ec17d10ecfeee2580900b0
Ramsy de Vos <ramsy@ramsy.dev>
2020-10-27 06:50:37 +0100

    Add configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,11 @@
 export default () => ({
+  server: {
+    scheme: process.env.SCHEME || 'http',
+    host: process.env.HOST || 'localhost',
     port: parseInt(process.env.PORT, 10) || 3000,
-    database: {
-      uri: process.env.MONGO_URI,
-      name: process.env.MONGO_DB_NAME
-    },
-  });
+  },
+  database: {
+    uri: process.env.MONGO_URI,
+    name: process.env.MONGO_DB_NAME
+  },
+})
